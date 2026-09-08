bc24d553fac1fae302fad7bdc46f13ec079d1225
Your Name <you@example.com>
2026-09-08 14:49:36 +0100

    Cambiar better-sqlite3 por node:sqlite

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-const Database = require("better-sqlite3");
+const { DatabaseSync } = require("node:sqlite");
 const path = require("path");
 
-const db = new Database(path.join(__dirname, "timely.db"));
-db.pragma("journal_mode = WAL");
-db.pragma("foreign_keys = ON");
+const db = new DatabaseSync(path.join(__dirname, "timely.db"));
+db.exec("PRAGMA journal_mode = WAL");
+db.exec("PRAGMA foreign_keys = ON");
 
 db.exec(`
 CREATE TABLE IF NOT EXISTS businesses (
